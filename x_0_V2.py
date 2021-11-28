24565029a51be20ce43863a0c6863563c29dbad6
sergei p <s1mple40rm@gmail.com>
2021-11-29 02:28:41 +0300

    well done

--- x_0_V2.py
+++ x_0_V2.py
@@ -1,16 +1,14 @@
 #import toc_tic
 
-data_x_0 = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
-#массив выбранных позиций
-chousen = []
+main_board = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
 
  
-def plot_table():
+def plot_board(board):
     #построение игрового поля
     for i in range(7):
         if i in [0, 3, 6]:
             print('-'*13)
-            print('| '+str(data_x_0[i+1])+ ' | '+str(data_x_0[i+2])+' | '+str(data_x_0[i+3])+' | ')
+            print('| '+str(board[i+1])+ ' | '+str(board[i+2])+' | '+str(board[i+3])+' | ')
     print('-'*13)
 
 def check_win(pl, board):
@@ -42,79 +40,95 @@ def check_win(pl, board):
     else:
         return 0
  
-def put_x(place):
-    data_x_0[place] = 'X'
+def put_x(place,board):
+    board[place] = 'X'
 
-def put_0(place):
-    data_x_0[place] = '0'
+def put_0(place,board):
+    board[place] = '0'
 
 def emptyIndices(board): # возвращает найденные пустые клетки
     availableindex = []
-    for i in range(1,len(board)):
-        if i !='X' and i !='0':
+    for i in range(1,len(board)+1):
+        if board[i] !='X' and board[i] !='0':
             availableindex.append(i) # добавляет в массив i
     return availableindex
+
+class Move:
+    def __init__(self,index,score):
+        self.index = index
+        self.score = score
 
 # функция минмакс
 def minmax(newboard, human, kampukter, curent_player):
     availspots = emptyIndices(newboard) # доступные клетки
-    if check_win(human, newboard): # проверка на поражение
-        return -1
-    if check_win(kampukter, newboard): # проверка на выйгрыш
-        return 1
+    if check_win(human, newboard): # проверка на поражение компьютера
+        return Move(-1,-1)
+    if check_win(kampukter, newboard): # проверка на выйгрыш компьютера
+        return Move(-1,1)
     if len(availspots) == 0: # проверка на ничью
-        return 0
+        return Move(-1,0)
     moves = [] # массив для хранения всех объектов
     for i in availspots: # цикл по доступным клеткам
-        move = {} # объект
+        print("check empty cell ",i)
+        move = Move(i,-1) # объект
         saveindex = newboard[i] # сохраняет i 
         newboard[i] = curent_player # далее ход за текущего игрока
         if curent_player == kampukter: # получаем очки 
+            print("try to make kampukter move on cell ",i)
             result = minmax(newboard, human, kampukter, human)
-            move[i] = result
+            print("got score ",result.score," for kampukter on cell ",i)
+            move.score = result.score
         else:
+            print("try to make player move on cell ",i)
             result = minmax(newboard, human, kampukter, kampukter)
-            move[i] = result
+            print("got score ",result.score," for player on cell ",i)
+            move.score = result.score
         newboard[i] = saveindex # очищаем клетку
         moves.append(move) # помещаем объект в массив
-    bestmove = {}
-    if curent_player == kampukter: 
-        bestscore = -10
-        for i in range(1,len(moves)): # пройти по ходам и выбрать ход с наибольшим кол-вом очков
-            if moves[i] > bestscore:
-                bestmove[i] = i
-            else:
-                bestscore = 10
-                for i in range(1,len(moves)): # пройти по ходам и выбрать ход с наименьшим кол-вом очков
-                    if moves[i] < bestscore:
-                        bestmove[i] = i
-    return moves[bestmove] # вернуть объект из массива ходов
+    
+    bestmove = Move(-1,-10)
+    if curent_player == kampukter:
+        for m in moves: # пройти по ходам и выбрать ход с наибольшим кол-вом очков
+            if m.score > bestmove.score:
+                bestmove = m
+    else:
+        bestmove.score = 10
+        for m in moves: # пройти по ходам и выбрать ход с наименьшим кол-вом очков
+            if  m.score < bestmove.score:
+                bestmove =  m
+    print("show current player moves:")
+    for m in moves:
+        print("index ",m.index," score ",m.score)
+ 
+    if bestmove.index == -1:
+        print("error index!!")
+        exit()
+    
+    print("best move is ",bestmove.index," with score ",bestmove.score)
+    return bestmove # вернуть объект из массива ходов
 
 
 # try|except позволяет вылавливать любые исклчения и их обрабатывать
-def chouse_place(player):
-    try:
-        place = input('Write number from 1 to 9 '+player+": ")
-        place = int(place)
-        if 1 <= place <= 9:
-            if place in chousen:
-                print('This place is not free')
-                return 1
-            else:
-                chousen.append(place)
-                if player == '0':
-                    put_0(place)
-                elif player =='X':
-                    put_x(place)
-                return 0
-        else:
-            print ('Write number from 1 to 9:')
-            return 1
-    except Exception as e: # вылавливает все исключения и их выводит
-        print(e)
-        return 1
+def choose_place(player,board):
+    while True:
+        try:
+            place = input('Write number from 1 to 9 '+player+": ")
+            place = int(place)
+            if 1 <= place <= 9:
+                if board[place] == 'X' or board[place] == '0':
+                    print('This place is not free')
+                else:
+                    if player == '0':
+                        put_0(place,board)
+                    elif player =='X':
+                        put_x(place,board)
+                    return
+        except Exception as e: # вылавливает все исключения и их выводит
+            print(e)
+        plot_board(board)
+        print ('Enter error, you should to write number from 1 to 9 suka!')
 
-def chouse_player():
+def choose_player():
     print('Choose who to play for Х or 0: ')
     human = input('Enter: ')
     kampukter = ''
@@ -128,8 +142,47 @@ def chouse_player():
         print('Mistake. There is no such player ')
     return human, kampukter
 
+def test_func():
+    test_board = {1: '1', 2: 'X', 3: 'X', 4: '0', 5: '5', 6: '0', 7: '7', 8: '0', 9: '9'}
+    plot_board(test_board)
+
+
 if __name__ == '__main__':
-    tmp =  minmax({1: '1', 2: 'X', 3: '3', 4: '0', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'},'0','X','X')
-    print(tmp)
-    # plot_table()
-exit()
+    human,kampukter = choose_player()
+    print('Your sign is ',human)
+    print('Kampukter sign is ',kampukter)
+
+    plot_board(main_board) #выводим состояние текущей доски
+    current_player = '' #выбираем кто ходит первый
+    if human == 'X':
+        current_player = human
+    else:
+        current_player = kampukter
+    
+    test_board = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+    game_over = False
+    while not game_over:
+        current_kampukter_move = minmax(test_board,human,kampukter,current_player)
+        if current_kampukter_move.index == -1:
+            print("Game over!")
+            game_over = True
+            if current_kampukter_move.score == 1:
+                print("Kampukter win!")
+            else:
+                if current_kampukter_move.score == -1:
+                    print("Player win!")
+                else:
+                    print("Fucking nichya!")
+        else:
+            plot_board(test_board)
+            if kampukter == current_player:
+                print("kampukter should to go in cell ",current_kampukter_move.index)
+                test_board[current_kampukter_move.index] = kampukter
+                current_player = human
+            else:
+                print("make your move suka!")
+                choose_place(human,test_board)
+                current_player = kampukter
+        plot_board(test_board)
+    exit()
+
